def899ff7ceb7953ae7e7a722af261c639023ad9
Luis Carlos <luiscarlos_ja@outlook.com>
2024-03-17 12:31:45 -0600

    Add API URL and implement fetching planets from the server

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,12 @@
+const API_URL = "http://localhost:8000";
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  try {
+    const response = await fetch(`${API_URL}/planets`);
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching planets", error);
+  }
 }
 
 async function httpGetLaunches() {
@@ -18,9 +24,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
