fac8fc025004ca736e26f0c825a87c23f60ba2e8
Luis Carlos <luiscarlos_ja@outlook.com>
2024-03-23 18:42:09 -0600

    Add planet and MongoDB integration

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,6 +1,7 @@
-const launches = new Map();
+const launches = require("./launches.mongo");
+const planets = require("./planets.mongo");
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
@@ -13,39 +14,69 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launches.findOne({
+    flightNumber: launchId,
+  });
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getLatestFlightNumber() {
+  const latestLaunch = await launches.findOne().sort("-flightNumber");
+  if (!latestLaunch) return DEFAULT_FLIGHT_NUMBER;
+  return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      flightNumber: latestFlightNumber,
-      customer: ["ZTM", "NASA"],
-      upcoming: true,
-      success: true,
-    })
+async function getAllLaunches() {
+  return await launches.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+  if (!planet) {
+    throw new Error("No matching planet was found");
+  }
+  await launches.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
   );
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ["NASA"],
+    flightNumber: newFlightNumber,
+  });
+  await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+  const aborted = await launches.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  scheduleNewLaunch,
   abortLaunchById,
 };
--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,90 +1,101 @@
 const request = require("supertest");
 const app = require("../../app");
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("Test GET /launches", () => {
-  it("GET /launches returns 200 status code", async () => {
-    const response = await request(app).get("/launches");
-    expect(response.statusCode).toBe(200);
+describe("Launches API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
   });
 
-  it("GET /launches returns an array", async () => {
-    const response = await request(app)
-      .get("/launches")
-      .expect("Content-Type", /json/);
-    expect(response.body).toBeInstanceOf(Array);
-  });
-});
-
-describe("Test POST /launch", () => {
-  const launchData = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    launchDate: "January 1, 2030",
-    target: "Kepler-186 f",
-  };
-
-  const launchDataWithoutDate = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    target: "Kepler-186 f",
-  };
-
-  const launchDataWithInvalidDate = {
-    mission: "USS Enterprise",
-    rocket: "NCC-1701-D",
-    launchDate: "Must to fail",
-    target: "Kepler-186 f",
-  };
-
-  it("POST /launches returns 201 status code", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchData)
-      .expect("Content-Type", /json/)
-      .expect(201);
-
-    const responseDate = new Date(response.body.launchDate).valueOf();
-    const requestData = new Date(launchData.launchDate).valueOf();
-
-    expect(responseDate).toBe(requestData);
-    expect(response.body).toMatchObject(launchDataWithoutDate);
+  afterAll(async () => {
+    await mongoDisconnect();
   });
 
-  it("POST /launches Missing required launch property returns 400 status code", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithoutDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
+  describe("Test GET /launches", () => {
+    it("GET /launches returns 200 status code", async () => {
+      const response = await request(app).get("/launches");
+      expect(response.statusCode).toBe(200);
+    });
 
-    expect(response.body).toStrictEqual({
-      error: "Missing required launch property",
+    it("GET /launches returns an array", async () => {
+      const response = await request(app)
+        .get("/launches")
+        .expect("Content-Type", /json/);
+      expect(response.body).toBeInstanceOf(Array);
     });
   });
 
-  it("POST /launches Invalid launch date return 400 status code", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(launchDataWithInvalidDate)
-      .expect("Content-Type", /json/)
-      .expect(400);
-    expect(response.body).toStrictEqual({ error: "Invalid launch date" });
-  });
-});
+  describe("Test POST /launch", () => {
+    const launchData = {
+      mission: "USS Enterprise",
+      rocket: "NCC-1701-D",
+      launchDate: "January 1, 2030",
+      target: "Kepler-62 f",
+    };
 
-describe("Test DELETE /launch/:id", () => {
-  it("DELETE /launches/:id returns 200 status code", async () => {
-    await request(app)
-      .delete("/launches/100")
-      .expect("Content-Type", /json/)
-      .expect(200);
+    const launchDataWithoutDate = {
+      mission: "USS Enterprise",
+      rocket: "NCC-1701-D",
+      target: "Kepler-62 f",
+    };
+
+    const launchDataWithInvalidDate = {
+      mission: "USS Enterprise",
+      rocket: "NCC-1701-D",
+      launchDate: "Must to fail",
+      target: "Kepler-62 f",
+    };
+
+    it("POST /launches returns 201 status code", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchData)
+        .expect("Content-Type", /json/)
+        .expect(201);
+
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      const requestData = new Date(launchData.launchDate).valueOf();
+
+      expect(responseDate).toBe(requestData);
+      expect(response.body).toMatchObject(launchDataWithoutDate);
+    });
+
+    it("POST /launches Missing required launch property returns 400 status code", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDataWithoutDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+
+      expect(response.body).toStrictEqual({
+        error: "Missing required launch property",
+      });
+    });
+
+    it("POST /launches Invalid launch date return 400 status code", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(launchDataWithInvalidDate)
+        .expect("Content-Type", /json/)
+        .expect(400);
+      expect(response.body).toStrictEqual({ error: "Invalid launch date" });
+    });
   });
 
-  it("DELETE /launches/:id returns 404 status code", async () => {
-    const response = await request(app)
-      .delete("/launches/102")
-      .expect("Content-Type", /json/)
-      .expect(404);
-    expect(response.body).toStrictEqual({ error: "Launch not found" });
+  describe("Test DELETE /launch/:id", () => {
+    it("DELETE /launches/:id returns 200 status code", async () => {
+      await request(app)
+        .delete("/launches/100")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+
+    it("DELETE /launches/:id returns 404 status code", async () => {
+      const response = await request(app)
+        .delete("/launches/102")
+        .expect("Content-Type", /json/)
+        .expect(404);
+      expect(response.body).toStrictEqual({ error: "Launch not found" });
+    });
   });
 });
