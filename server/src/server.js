2648a8b05cd034e1efee006ba152d204c2145983
Luis Carlos <luiscarlos_ja@outlook.com>
2024-03-24 19:15:08 -0600

    Add axios dependency and update launch data retrieval

--- server/src/server.js
+++ server/src/server.js
@@ -3,14 +3,16 @@ const http = require("http");
 const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
-  mongoConnect();
+  await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
